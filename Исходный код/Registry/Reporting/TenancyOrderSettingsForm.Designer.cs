c28e92758cca21930d24e743f97fab058361d574
IgnVV <ignvv@bratsk-city.ru>
2016-05-18 15:11:41 +0800

    Распоряжение по переселению разбито на два: "по сводным спискам" и "перераспределение".
    Signed-off-by: IgnVV <ignvv@bratsk-city.ru>

--- Исходный код/Registry/Reporting/TenancyOrderSettingsForm.Designer.cs
+++ Исходный код/Registry/Reporting/TenancyOrderSettingsForm.Designer.cs
@@ -46,7 +46,7 @@
             this.label4 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.dateTimePickerOrderFrom = new System.Windows.Forms.DateTimePicker();
-            this.checkBoxResettle2013to2017 = new System.Windows.Forms.CheckBox();
+            this.checkBoxResettle2013to2017freeLists = new System.Windows.Forms.CheckBox();
             this.textBoxHouse = new System.Windows.Forms.TextBox();
             this.label5 = new System.Windows.Forms.Label();
             this.label6 = new System.Windows.Forms.Label();
@@ -54,6 +54,7 @@
             this.groupBoxAddress = new System.Windows.Forms.GroupBox();
             this.checkBoxEnableAddress = new System.Windows.Forms.CheckBox();
             this.checkBoxCheckanovskiy = new System.Windows.Forms.CheckBox();
+            this.checkBoxResettle2013to2017retransfer = new System.Windows.Forms.CheckBox();
             this.groupBoxAddress.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -62,7 +63,7 @@
             this.vButtonOk.AllowAnimations = true;
             this.vButtonOk.BackColor = System.Drawing.Color.Transparent;
             this.vButtonOk.DialogResult = System.Windows.Forms.DialogResult.OK;
-            this.vButtonOk.Location = new System.Drawing.Point(12, 473);
+            this.vButtonOk.Location = new System.Drawing.Point(45, 493);
             this.vButtonOk.Name = "vButtonOk";
             this.vButtonOk.RoundedCornersMask = ((byte)(15));
             this.vButtonOk.Size = new System.Drawing.Size(117, 35);
@@ -76,7 +77,7 @@
             this.vButtonCancel.AllowAnimations = true;
             this.vButtonCancel.BackColor = System.Drawing.Color.Transparent;
             this.vButtonCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-            this.vButtonCancel.Location = new System.Drawing.Point(147, 473);
+            this.vButtonCancel.Location = new System.Drawing.Point(180, 493);
             this.vButtonCancel.Name = "vButtonCancel";
             this.vButtonCancel.RoundedCornersMask = ((byte)(15));
             this.vButtonCancel.Size = new System.Drawing.Size(117, 35);
@@ -87,15 +88,15 @@
             // 
             // textBoxProtocolNum
             // 
-            this.textBoxProtocolNum.Location = new System.Drawing.Point(15, 27);
+            this.textBoxProtocolNum.Location = new System.Drawing.Point(20, 27);
             this.textBoxProtocolNum.Name = "textBoxProtocolNum";
-            this.textBoxProtocolNum.Size = new System.Drawing.Size(248, 21);
+            this.textBoxProtocolNum.Size = new System.Drawing.Size(294, 21);
             this.textBoxProtocolNum.TabIndex = 0;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(15, 10);
+            this.label2.Location = new System.Drawing.Point(20, 10);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(111, 15);
             this.label2.TabIndex = 56;
@@ -104,7 +105,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(15, 51);
+            this.label1.Location = new System.Drawing.Point(20, 51);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(102, 15);
             this.label1.TabIndex = 58;
@@ -112,24 +113,24 @@
             // 
             // dateTimePickerProtocolDate
             // 
-            this.dateTimePickerProtocolDate.Location = new System.Drawing.Point(15, 68);
+            this.dateTimePickerProtocolDate.Location = new System.Drawing.Point(20, 68);
             this.dateTimePickerProtocolDate.Name = "dateTimePickerProtocolDate";
-            this.dateTimePickerProtocolDate.Size = new System.Drawing.Size(248, 21);
+            this.dateTimePickerProtocolDate.Size = new System.Drawing.Size(294, 21);
             this.dateTimePickerProtocolDate.TabIndex = 1;
             // 
             // comboBoxRentType
             // 
             this.comboBoxRentType.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.comboBoxRentType.FormattingEnabled = true;
-            this.comboBoxRentType.Location = new System.Drawing.Point(15, 109);
+            this.comboBoxRentType.Location = new System.Drawing.Point(20, 109);
             this.comboBoxRentType.Name = "comboBoxRentType";
-            this.comboBoxRentType.Size = new System.Drawing.Size(249, 23);
+            this.comboBoxRentType.Size = new System.Drawing.Size(295, 23);
             this.comboBoxRentType.TabIndex = 2;
             // 
             // label73
             // 
             this.label73.AutoSize = true;
-            this.label73.Location = new System.Drawing.Point(15, 92);
+            this.label73.Location = new System.Drawing.Point(20, 92);
             this.label73.Name = "label73";
             this.label73.Size = new System.Drawing.Size(68, 15);
             this.label73.TabIndex = 61;
@@ -137,15 +138,15 @@
             // 
             // dateTimePickerRegistrationTo
             // 
-            this.dateTimePickerRegistrationTo.Location = new System.Drawing.Point(44, 266);
+            this.dateTimePickerRegistrationTo.Location = new System.Drawing.Point(49, 266);
             this.dateTimePickerRegistrationTo.Name = "dateTimePickerRegistrationTo";
-            this.dateTimePickerRegistrationTo.Size = new System.Drawing.Size(220, 21);
+            this.dateTimePickerRegistrationTo.Size = new System.Drawing.Size(266, 21);
             this.dateTimePickerRegistrationTo.TabIndex = 8;
             // 
             // label9
             // 
             this.label9.AutoSize = true;
-            this.label9.Location = new System.Drawing.Point(20, 269);
+            this.label9.Location = new System.Drawing.Point(25, 269);
             this.label9.Name = "label9";
             this.label9.Size = new System.Drawing.Size(21, 15);
             this.label9.TabIndex = 7;
@@ -154,7 +155,7 @@
             // label10
             // 
             this.label10.AutoSize = true;
-            this.label10.Location = new System.Drawing.Point(22, 239);
+            this.label10.Location = new System.Drawing.Point(27, 239);
             this.label10.Name = "label10";
             this.label10.Size = new System.Drawing.Size(13, 15);
             this.label10.TabIndex = 5;
@@ -162,15 +163,15 @@
             // 
             // dateTimePickerRegistrationFrom
             // 
-            this.dateTimePickerRegistrationFrom.Location = new System.Drawing.Point(44, 236);
+            this.dateTimePickerRegistrationFrom.Location = new System.Drawing.Point(49, 236);
             this.dateTimePickerRegistrationFrom.Name = "dateTimePickerRegistrationFrom";
-            this.dateTimePickerRegistrationFrom.Size = new System.Drawing.Size(220, 21);
+            this.dateTimePickerRegistrationFrom.Size = new System.Drawing.Size(266, 21);
             this.dateTimePickerRegistrationFrom.TabIndex = 6;
             // 
             // label11
             // 
             this.label11.AutoSize = true;
-            this.label11.Location = new System.Drawing.Point(15, 219);
+            this.label11.Location = new System.Drawing.Point(20, 219);
             this.label11.Name = "label11";
             this.label11.Size = new System.Drawing.Size(114, 15);
             this.label11.TabIndex = 67;
@@ -180,15 +181,15 @@
             // 
             this.comboBoxExecutor.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.comboBoxExecutor.FormattingEnabled = true;
-            this.comboBoxExecutor.Location = new System.Drawing.Point(15, 152);
+            this.comboBoxExecutor.Location = new System.Drawing.Point(20, 152);
             this.comboBoxExecutor.Name = "comboBoxExecutor";
-            this.comboBoxExecutor.Size = new System.Drawing.Size(249, 23);
+            this.comboBoxExecutor.Size = new System.Drawing.Size(295, 23);
             this.comboBoxExecutor.TabIndex = 3;
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(15, 135);
+            this.label4.Location = new System.Drawing.Point(20, 135);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(85, 15);
             this.label4.TabIndex = 73;
@@ -197,7 +198,7 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(15, 178);
+            this.label3.Location = new System.Drawing.Point(20, 178);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(110, 15);
             this.label3.TabIndex = 62;
@@ -205,29 +206,29 @@
             // 
             // dateTimePickerOrderFrom
             // 
-            this.dateTimePickerOrderFrom.Location = new System.Drawing.Point(15, 195);
+            this.dateTimePickerOrderFrom.Location = new System.Drawing.Point(20, 195);
             this.dateTimePickerOrderFrom.MinDate = new System.DateTime(2005, 1, 1, 0, 0, 0, 0);
             this.dateTimePickerOrderFrom.Name = "dateTimePickerOrderFrom";
-            this.dateTimePickerOrderFrom.Size = new System.Drawing.Size(249, 21);
+            this.dateTimePickerOrderFrom.Size = new System.Drawing.Size(295, 21);
             this.dateTimePickerOrderFrom.TabIndex = 4;
             // 
-            // checkBoxResettle2013to2017
+            // checkBoxResettle2013to2017freeLists
             // 
-            this.checkBoxResettle2013to2017.AutoSize = true;
-            this.checkBoxResettle2013to2017.Location = new System.Drawing.Point(14, 417);
-            this.checkBoxResettle2013to2017.Name = "checkBoxResettle2013to2017";
-            this.checkBoxResettle2013to2017.Size = new System.Drawing.Size(252, 19);
-            this.checkBoxResettle2013to2017.TabIndex = 11;
-            this.checkBoxResettle2013to2017.Text = "По программе переселения 2013-2017";
-            this.checkBoxResettle2013to2017.UseVisualStyleBackColor = true;
-            this.checkBoxResettle2013to2017.CheckStateChanged += new System.EventHandler(this.checkBoxResettle2013to2017_CheckedChanged);
+            this.checkBoxResettle2013to2017freeLists.AutoSize = true;
+            this.checkBoxResettle2013to2017freeLists.Location = new System.Drawing.Point(19, 417);
+            this.checkBoxResettle2013to2017freeLists.Name = "checkBoxResettle2013to2017freeLists";
+            this.checkBoxResettle2013to2017freeLists.Size = new System.Drawing.Size(298, 19);
+            this.checkBoxResettle2013to2017freeLists.TabIndex = 10;
+            this.checkBoxResettle2013to2017freeLists.Text = "Переселение 2013-2017 (по сводным спискам)";
+            this.checkBoxResettle2013to2017freeLists.UseVisualStyleBackColor = true;
+            this.checkBoxResettle2013to2017freeLists.CheckStateChanged += new System.EventHandler(this.checkBoxResettle2013to2017freeLists_CheckedChanged);
             // 
             // textBoxHouse
             // 
             this.textBoxHouse.Location = new System.Drawing.Point(14, 82);
             this.textBoxHouse.MaxLength = 4;
             this.textBoxHouse.Name = "textBoxHouse";
-            this.textBoxHouse.Size = new System.Drawing.Size(248, 21);
+            this.textBoxHouse.Size = new System.Drawing.Size(295, 21);
             this.textBoxHouse.TabIndex = 1;
             // 
             // label5
@@ -253,7 +254,7 @@
             this.comboBoxStreet.FormattingEnabled = true;
             this.comboBoxStreet.Location = new System.Drawing.Point(14, 38);
             this.comboBoxStreet.Name = "comboBoxStreet";
-            this.comboBoxStreet.Size = new System.Drawing.Size(248, 23);
+            this.comboBoxStreet.Size = new System.Drawing.Size(295, 23);
             this.comboBoxStreet.TabIndex = 0;
             this.comboBoxStreet.DropDownClosed += new System.EventHandler(this.comboBoxStreet_DropDownClosed);
             this.comboBoxStreet.KeyUp += new System.Windows.Forms.KeyEventHandler(this.comboBoxStreet_KeyUp);
@@ -266,17 +267,17 @@
             this.groupBoxAddress.Controls.Add(this.label5);
             this.groupBoxAddress.Controls.Add(this.label6);
             this.groupBoxAddress.Enabled = false;
-            this.groupBoxAddress.Location = new System.Drawing.Point(1, 293);
+            this.groupBoxAddress.Location = new System.Drawing.Point(6, 293);
             this.groupBoxAddress.Name = "groupBoxAddress";
-            this.groupBoxAddress.Size = new System.Drawing.Size(276, 116);
-            this.groupBoxAddress.TabIndex = 10;
+            this.groupBoxAddress.Size = new System.Drawing.Size(325, 116);
+            this.groupBoxAddress.TabIndex = 9;
             this.groupBoxAddress.TabStop = false;
             this.groupBoxAddress.Text = "      Адрес";
             // 
             // checkBoxEnableAddress
             // 
             this.checkBoxEnableAddress.AutoSize = true;
-            this.checkBoxEnableAddress.Location = new System.Drawing.Point(11, 295);
+            this.checkBoxEnableAddress.Location = new System.Drawing.Point(16, 295);
             this.checkBoxEnableAddress.Name = "checkBoxEnableAddress";
             this.checkBoxEnableAddress.Size = new System.Drawing.Size(15, 14);
             this.checkBoxEnableAddress.TabIndex = 9;
@@ -286,7 +287,7 @@
             // checkBoxCheckanovskiy
             // 
             this.checkBoxCheckanovskiy.AutoSize = true;
-            this.checkBoxCheckanovskiy.Location = new System.Drawing.Point(14, 442);
+            this.checkBoxCheckanovskiy.Location = new System.Drawing.Point(19, 467);
             this.checkBoxCheckanovskiy.Name = "checkBoxCheckanovskiy";
             this.checkBoxCheckanovskiy.Size = new System.Drawing.Size(243, 19);
             this.checkBoxCheckanovskiy.TabIndex = 12;
@@ -294,16 +295,28 @@
             this.checkBoxCheckanovskiy.UseVisualStyleBackColor = true;
             this.checkBoxCheckanovskiy.CheckStateChanged += new System.EventHandler(this.checkBoxCheckanovskiy_CheckedChanged);
             // 
+            // checkBoxResettle2013to2017retransfer
+            // 
+            this.checkBoxResettle2013to2017retransfer.AutoSize = true;
+            this.checkBoxResettle2013to2017retransfer.Location = new System.Drawing.Point(19, 442);
+            this.checkBoxResettle2013to2017retransfer.Name = "checkBoxResettle2013to2017retransfer";
+            this.checkBoxResettle2013to2017retransfer.Size = new System.Drawing.Size(296, 19);
+            this.checkBoxResettle2013to2017retransfer.TabIndex = 11;
+            this.checkBoxResettle2013to2017retransfer.Text = "Переселение 2013-2017 (перераспределение)";
+            this.checkBoxResettle2013to2017retransfer.UseVisualStyleBackColor = true;
+            this.checkBoxResettle2013to2017retransfer.CheckedChanged += new System.EventHandler(this.checkBoxResettle2013to2017retransfer_CheckedChanged);
+            // 
             // TenancyOrderSettingsForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(190)))), ((int)(((byte)(208)))), ((int)(((byte)(235)))));
-            this.ClientSize = new System.Drawing.Size(279, 519);
+            this.ClientSize = new System.Drawing.Size(338, 533);
+            this.Controls.Add(this.checkBoxResettle2013to2017retransfer);
             this.Controls.Add(this.checkBoxCheckanovskiy);
             this.Controls.Add(this.checkBoxEnableAddress);
             this.Controls.Add(this.groupBoxAddress);
-            this.Controls.Add(this.checkBoxResettle2013to2017);
+            this.Controls.Add(this.checkBoxResettle2013to2017freeLists);
             this.Controls.Add(this.comboBoxExecutor);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.dateTimePickerRegistrationTo);
@@ -355,7 +368,7 @@
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.DateTimePicker dateTimePickerOrderFrom;
-        private System.Windows.Forms.CheckBox checkBoxResettle2013to2017;
+        private System.Windows.Forms.CheckBox checkBoxResettle2013to2017freeLists;
         private System.Windows.Forms.TextBox textBoxHouse;
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.Label label6;
@@ -363,5 +376,6 @@
         private System.Windows.Forms.GroupBox groupBoxAddress;
         private System.Windows.Forms.CheckBox checkBoxEnableAddress;
         private System.Windows.Forms.CheckBox checkBoxCheckanovskiy;
+        private System.Windows.Forms.CheckBox checkBoxResettle2013to2017retransfer;
     }
 }
