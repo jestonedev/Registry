6804f33a7315de3c828c710ed12edb1aa9450dcb
IgnVV <ignvv@bratsk-city.ru>
2016-05-24 16:54:37 +0800

    Исправил исправление импорта из МСП (сделал его немного более гибким)

--- Исходный код/Registry/Viewports/TenancyPersonsViewport.cs
+++ Исходный код/Registry/Viewports/TenancyPersonsViewport.cs
@@ -848,7 +848,7 @@ namespace Registry.Viewport
                           ELSE ts.base_month
                           END AS mon, family, name, father, birth, c AS reg_date
                         FROM dbo.t_bks ts
-                        WHERE ts.street = @street AND house = @house AND flat IN (@flat, 'ком.'+@flat, 'ком'+@flat, 'ком. '+@flat)) v
+                        WHERE ts.street = @street AND house = @house AND RTRIM(LTRIM(REPLACE(REPLACE(flat,'ком.',''),'ком',''))) = @flat) v
                         WHERE mon = (
                           SELECT 
                           MAX(CASE SUBSTRING(ts.base_month,1,3) 
@@ -867,8 +867,7 @@ namespace Registry.Viewport
                           ELSE ts.base_month
                           END) AS mon
                           FROM dbo.t_bks ts
-                          WHERE ts.street = @street AND house = @house AND flat IN (@flat, 'ком.'+@flat, 'ком'+@flat, 'ком. '+@flat)
-                          )
+                          WHERE ts.street = @street AND house = @house AND RTRIM(LTRIM(REPLACE(REPLACE(flat,'ком.',''),'ком',''))) = @flat)
                         ORDER BY mon;"
                     };
                     var streetName = currentPremise.First().street_name;
